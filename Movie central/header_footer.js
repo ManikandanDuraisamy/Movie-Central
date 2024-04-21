206acf41b1930bd889872d89656abfe24f87b762
Manikandan Duraisamy <rbzmani@gmail.com>
2024-04-21 18:18:59 +0200

    Updated

--- Movie central/header_footer.js
+++ Movie central/header_footer.js
@@ -1,48 +1,55 @@
 class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
-              <div class="sticky-top">
+      <div class="sticky-top">
       <header>
-      <img src="https://group01moviecinema.s3.eu-central-1.amazonaws.com/header_footer_images/Header+3.jpg" class="card-img" height="150px" alt="header">
+      <img src="https://group01moviecinema.s3.eu-central-1.amazonaws.com/header_footer_images/Header+1.jpg" class="card-img" height="200px" alt="header">
       <div class="card-img-overlay">
-          <br>
+      
+      
       <img src="https://group01moviecinema.s3.eu-central-1.amazonaws.com/header_footer_images/Movie+Central+1.JPG" width="100px" height="80px"  alt="Movie Central">
       <br>
-          <nav class="navbar navbar-expand-lg bg-body-tertiary" >
+      <br>
+      <br>
+      <br>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary" >
               <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarScroll">
                   <ul class="navbar-nav me-auto my-1 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                       <li class="nav-item">
-                          <a class="nav-link" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/main_pages_html/movies_main.html">Home</a>
+                          <a class="nav-link" href="">Home</a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/main_pages_html/cinemas_main.html">Cinemas</a>
+                          <a class="nav-link" href="cinemas_main.html">Cinemas</a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="#">Booking</a>
+                          <a class="nav-link" href="movies_main.html">Movies</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="reviews_main.html">Reviews</a>
                       </li>
                       <li class="nav-item dropdown">
                       <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                       Administrator
                       </a>
                       <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/movies_page.html">Movies</a></li>
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/cinemas_page.html">Cinemas</a></li>
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/Schedules_page.html">Schedule</a></li>
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/users_page(get%26post)-2.html">User</a></li>
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/reviews_page-2.html">Review</a></li>
-                        <li><a class="dropdown-item" href="https://group01moviecinema.s3.eu-central-1.amazonaws.com/html+files/administrator_html/bookings_page.html">Booking</a></li>
+                        <li><a class="dropdown-item" href="movies_page.html">Movies</a></li>
+                        <li><a class="dropdown-item" href="cinemas_page.html">Cinemas</a></li>
+                        <li><a class="dropdown-item" href="Schedules_page.html">Schedule</a></li>
+                        <li><a class="dropdown-item" href="users_page(get%26post)-2.html">User</a></li>
+                        <li><a class="dropdown-item" href="reviews_page-2.html">Review</a></li>
+                        <li><a class="dropdown-item" href="bookings_page.html">Booking</a></li>
                       </ul>
                     </li>
                   </ul>
-                  <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                  </form>
+                  <input class="form-control1 me-3" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
                 </div>
               </div>
             </nav>
       </div>
+      
+      
       </header>
       </div>
           `
@@ -58,28 +65,26 @@ class Header extends HTMLElement {
           <footer>
           <p>&copy; 2024 Movie Central. All rights reserved.</p>
           <div class="card text-bg-dark">
-              <img src="https://group01moviecinema.s3.eu-central-1.amazonaws.com/header_footer_images/Header+3.jpg" class="card-img" height="200px" alt="footer">
+              <img src="https://group01moviecinema.s3.eu-central-1.amazonaws.com/header_footer_images/Header+1.jpg" class="card-img" height="200px" alt="footer">
               <div class="card-img-overlay">
-                <h3 class="card-title">Movie Central</h3><br> 
-                <p class="card-text">Save time, save money! <br> 
-                  Sign up and we'll send the best deals to you</p>  
-                  <form>
-                <div class="mb-3">
-                    <label for="emailAddress" class="form-label">Email Address</label>
-                    <input type="text" class="form-control" id="email" name="email" required>
-                <button type="submit" class="btn btn-primary">Register</button>
-                </div>
-                </form>
-                  <p class="card-text"><small>moviecentral@gmail.com
-                    <br> 
-                    24/7 <br>
-                  </small></p>    
+                <figure class="blockquote">
+                <blockquote class="blockquote">
+                  <p>Contact Us @ moviecentral@gmail.com</p>
+                </blockquote>
+              </figure> 
+               
+                  <figure class="text-end card-img-overlay">
+                    <blockquote class="blockquote">
+                    <p>Save time & save money with Movie Central!</p>
                   
-                
-                
-                     
-              </div>
-            
+                    <p>Sign up and we'll send the best deals to you!</p>
+                    <label for="emailAddress" class="form-label">Email Address</label>
+                    <input type="text" id="email" name="email" required><br>  
+                <button type="button" class="btn btn-secondary" >Register</button>
+                </blockquote>
+                  </figure>   
+               </div>
+          </div>
       </footer>`
     }
   }
